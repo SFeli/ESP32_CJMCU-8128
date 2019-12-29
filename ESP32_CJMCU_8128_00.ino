dce49ebe2a25043daacaf21697e449d6040cce08
SFeli <GitHub@felmayer.de>
2019-12-29 12:59:33 +0100

    Changed Library CCS811 from Adafriut_CCS811 to Maartens'

--- ESP32_CJMCU_8128_00.ino
+++ ESP32_CJMCU_8128_00.ino
@@ -1,16 +1,23 @@
-/* Lolin D32
-    Sensor CJMCU 8128 mit:
+/* ESP32
+    Sensor CJMCU-8128 mit:
     CCS811 -> 0x5A !!!
-      bool begin(addr)
-      setDriverMode(Mode 1sec, 10sec, 60 sec, 250 msec)
-      bool available()
-      setEnvironmentalData(humidity, temperature)
-      double calculateTemperature()
-      setThresholds(low,med,hysteresis)
-      SWReset()
-      bool checkError()
-      _i2c_init()
-      setTempOffset(float) ?
+      bool begin()
+      bool start(mode)
+      read(eco2, etvoc, errstat, raw)
+      int hardware_version()
+      int bootloader_version()
+      int application_version()
+      int get_errorid()
+      bool set_envdate(t,h)
+      bool set_envdata210(t,h)
+      bool get_baseline(baseline)
+      bool set_baseline(baseline)
+      bool flash(image, size)
+      set_i2cdelay(us)
+      get_i2cdelay(us)
+      wake_init()
+      wake_up()
+      wake_down()
     SI7021 -> 0x40
       float readHumidity()
       float readTemperature()
@@ -29,25 +36,33 @@
 */
 
 #include <Arduino.h>
-#include "Adafruit_CCS811.h"            // include main library for CCS811 - Sensor
+#include "ccs811.h"                     // include library for CCS811 - Sensor from martin-pennings https://github.com/maarten-pennings/CCS811
 #include "Adafruit_Si7021.h"            // include main library for SI7021 - Sensor
 #include "Adafruit_BMP280.h"            // include main library for BMP280 - Sensor
 #include <Wire.h>                       // This library allows you to communicate with I2C
 
-Adafruit_CCS811 ccs811;
+CCS811 ccs811;
 Adafruit_BMP280 bmp280;                // I2C
 Adafruit_Si7021 SI702x = Adafruit_Si7021();
 
 void setup() {
   Serial.begin(9600);
-
+  // Enable I2C
+  Wire.begin();
   Serial.println("CCS811 test");      /* --- SETUP CCS811 on 0x5A ------ */
-  if (!ccs811.begin(0x5A)) {
+  ccs811.set_i2cdelay(50); // Needed for ESP8266 because it doesn't handle I2C clock stretch correctly
+  if (!ccs811.begin()) {
     Serial.println("Failed to start sensor! Please check your wiring.");
     while (true);
   }
-  // Wait for the sensor to be ready
-  while (!ccs811.available());
+  // Print CCS811 versions
+  Serial.print("setup: hardware    version: "); Serial.println(ccs811.hardware_version(), HEX);
+  Serial.print("setup: bootloader  version: "); Serial.println(ccs811.bootloader_version(), HEX);
+  Serial.print("setup: application version: "); Serial.println(ccs811.application_version(), HEX);
+
+  // Start measuring
+  bool ok = ccs811.start(CCS811_MODE_1SEC);
+  if ( !ok ) Serial.println("setup: CCS811 start FAILED");
 
   Serial.println("BMP280 test");     /* --- SETUP BMP on 0x76 ------ */
   if (!bmp280.begin(0x76)) {
@@ -89,9 +104,9 @@ void loop() {
   Serial.print(bmp280.readPressure() / 100);
   Serial.println(" Pa, ");
 
-//  Serial.print("Approx altitude = ");
-//  Serial.print(bmp280.readAltitude(1013.25));        /* Adjusted to local forecast! */
-//  Serial.println(" m");
+  //  Serial.print("Approx altitude = ");
+  //  Serial.print(bmp280.readAltitude(1013.25));        /* Adjusted to local forecast! */
+  //  Serial.println(" m");
 
   Serial.print("SI702x => Temperature = ");
   Serial.print(SI702x.readTemperature(), 2);
@@ -99,12 +114,15 @@ void loop() {
   Serial.print("Humidity = ");
   Serial.println(SI702x.readHumidity(), 2);
 
-  ccs811.setEnvironmentalData(SI702x.readHumidity(), bmp280.readTemperature());
-  if (!ccs811.readData()) {
+  uint16_t eco2, etvoc, errstat, raw;     // Read CCS811
+
+  ccs811.set_envdata(bmp280.readTemperature(), SI702x.readHumidity());
+  ccs811.read(&eco2, &etvoc, &errstat, &raw);
+  if ( errstat == CCS811_ERRSTAT_OK ) {
     Serial.print("CCS811 => CO2 = ");
-    Serial.print(ccs811.geteCO2());
+    Serial.print(eco2);
     Serial.print("ppm, TVOC = ");
-    Serial.println(ccs811.getTVOC());
+    Serial.println(etvoc);
   }
   delay(5000);
 }
